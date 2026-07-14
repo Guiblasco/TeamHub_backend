1cad807ca075d5dcf5df7136cb44c05634a5925f
Guillem Blasco <gowet92@gmail.com>
2026-07-14 10:58:47 +0200

    feat(swagger): decorate health controller

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
-import { PrismaService } from '../../common/prisma/prisma.service';
-
+import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
+@ApiTags('Health')
 @Controller('health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
   @Get()
+  @ApiOperation({
+    summary: 'Check application health',
+  })
+  @ApiOkResponse({
+    description: 'Application is running correctly',
+  })
   check() {
     return this.healthService.check();
   }
